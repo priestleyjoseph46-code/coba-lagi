c43b04d55e403f32ac83cfe2606d9c027f5df914
priestleyjoseph46-code <priestleyjoseph46@gmail.com>
2025-08-15 00:31:04 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    viteSourceLocator({
-      prefix: "coba-lagi",
-    }),
+export default defineConfig({
+  base: 'coba-lagi', // Replace with your actual repository name
+  plugins: [react()],
+});
+
     react(),
   resolve: {
     alias: {
